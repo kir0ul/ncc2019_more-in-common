8c1787b43dbe5aa8a7b96b09bf5e5c561d329f18
Andrea Pierré <573432-apierre@users.noreply.gitlab.com>
2019-03-18 00:47:01 +0100

    Serve static wordcloud file from API

--- back/app.py
+++ back/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-import json
-from flask.json import jsonify
+# import json
+# from flask.json import jsonify
 import plotly
 import plotly.graph_objs as go
 from plotly.offline import plot
 import random
-import plotly.io as pio
+# import plotly.io as pio
 import pandas as pd
+from flask import render_template
+import os
 
 app = Flask(__name__)
 
@@ -19,7 +21,7 @@ NAMES = ["words", "tweets"]
 WORDS_DF = pd.read_csv(WORDCLOUD_FILE_PATH, names=NAMES)
 WORDS = WORDS_DF[NAMES[0]]
 WEIGHTS = WORDS_DF[NAMES[1]]
-WEIGHTS = WEIGHTS*100/WEIGHTS.max()+20
+WEIGHTS = WEIGHTS * 100 / WEIGHTS.max() + 20
 
 
 @app.route("/")
@@ -32,18 +34,25 @@ def api_root():
 def get_hashtag(hashtag):
     """Hashtag route"""
 
-    # Mocked JSON
-    mocked_json_path = "../data/api_out.json"
-    with open(mocked_json_path, "r") as fh:
-        data = fh.readlines()
-        data = "".join(data)
-    response = json.loads(data)
-    response = jsonify(response)
+    # # Mocked JSON
+    # mocked_json_path = "../data/api_out.json"
+    # with open(mocked_json_path, "r") as fh:
+    #     data = fh.readlines()
+    #     data = "".join(data)
+    # response = json.loads(data)
+    # response = jsonify(response)
+    # return response
 
-    return response
+    templates_folder = "templates"
+    html_plot_name = "plot.html"
+    html_plot_path = os.path.join(templates_folder, html_plot_name)
+    if not os.path.exists(templates_folder):
+        os.mkdir(templates_folder)
+    generate_wordcloud(plot_path=html_plot_path)
+    return render_template(html_plot_name)
 
 
-def generate_wordcloud(words=WORDS, weights=WEIGHTS):
+def generate_wordcloud(words=WORDS, weights=WEIGHTS, plot_path=""):
 
     colors = [
         plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)]
@@ -73,10 +82,9 @@ def generate_wordcloud(words=WORDS, weights=WEIGHTS):
         }
     })
     fig = go.Figure(data=[data], layout=layout)
-
-    plot(fig)
-    pio.write_image(fig, 'fig.svg')
+    plot(fig, filename=plot_path, auto_open=False)
+    # pio.write_image(fig, 'fig.svg')
 
 
 if __name__ == '__main__':
-    generate_wordcloud()
+    app.run(debug=True)
