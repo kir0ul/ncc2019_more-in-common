5b403036d19c98b39218d918e70c6cedf4bafedc
Andrea Pierré <andrea.pierre@sewan.fr>
2019-03-17 11:51:27 +0100

    Emergency solution to get a wordcloud plot

--- back/app.py
+++ back/app.py
@@ -1,8 +1,25 @@
 from flask import Flask
 import json
 from flask.json import jsonify
+import plotly
+import plotly.graph_objs as go
+from plotly.offline import plot
+import random
+import plotly.io as pio
+import pandas as pd
 
 app = Flask(__name__)
+
+# ---------- Hard coded data ----------
+WORDS = dir(go)[:30]
+WEIGHTS = [random.randint(15, 35) for i in range(30)]
+
+WORDCLOUD_FILE_PATH = "../data/emergency.csv"
+NAMES = ["words", "tweets"]
+WORDS_DF = pd.read_csv(WORDCLOUD_FILE_PATH, names=NAMES)
+WORDS = WORDS_DF[NAMES[0]]
+WEIGHTS = WORDS_DF[NAMES[1]]
+WEIGHTS = WEIGHTS*100/WEIGHTS.max()+20
 
 
 @app.route("/")
@@ -24,3 +41,42 @@ def get_hashtag(hashtag):
     response = jsonify(response)
 
     return response
+
+
+def generate_wordcloud(words=WORDS, weights=WEIGHTS):
+
+    colors = [
+        plotly.colors.DEFAULT_PLOTLY_COLORS[random.randrange(1, 10)]
+        for i in range(30)
+    ]
+
+    data = go.Scatter(
+        x=[random.random() for i in range(30)],
+        y=[random.random() for i in range(30)],
+        mode='text',
+        text=words,
+        marker={'opacity': 0.3},
+        textfont={
+            'size': weights,
+            'color': colors
+        })
+    layout = go.Layout({
+        'xaxis': {
+            'showgrid': False,
+            'showticklabels': False,
+            'zeroline': False
+        },
+        'yaxis': {
+            'showgrid': False,
+            'showticklabels': False,
+            'zeroline': False
+        }
+    })
+    fig = go.Figure(data=[data], layout=layout)
+
+    plot(fig)
+    pio.write_image(fig, 'fig.svg')
+
+
+if __name__ == '__main__':
+    generate_wordcloud()
