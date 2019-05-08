f9ff5074df93ab243cc420d9e24d9ecf7f2163af
Andrea Pierré <573432-apierre@users.noreply.gitlab.com>
2019-05-08 18:01:24 +0200

    [POC] Implement reactive table based on text input

--- dash/app.py
+++ dash/app.py
@@ -1,49 +1,64 @@
+"""Dash interface"""
+
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_table
 import pandas as pd
 
-
-df = pd.read_csv("./data/usa-agricultural-exports-2011.csv")
-
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table(
-        # Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])]
-        +
-        # Body
-        [
-            html.Tr([html.Td(dataframe.iloc[i][col]) for col in dataframe.columns])
-            for i in range(min(len(dataframe), max_rows))
-        ]
-    )
+DF_EN = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+DF_FR = DF_EN.copy(deep=True)
+DF_FR.rename(columns={
+    'State': 'Etat',
+    'Installed Capacity (MW)': 'Capacite installee (MW)',
+    "Number of Solar Plants": "Nombre d'installations solaires"
+},
+             inplace=True)
 
 
-app = dash.Dash(__name__)
-app.layout = html.Div(
+def generate_table(dataframe):
+    """Generate table from DataFrame"""
+    return dash_table.DataTable(id="table",
+                                columns=[{
+                                    "name": i,
+                                    "id": i
+                                } for i in dataframe.columns],
+                                data=dataframe.to_dict('records'))
+
+
+APP = dash.Dash(__name__)
+APP.layout = html.Div([
+    html.Div(id="output-container-button",
+             children="Enter a hashtag and press submit"),
+    html.Div(
+        dcc.Input(id="input-box", type="text", placeholder='Search',
+                  value='')),
+    html.Button("Submit", id="button"),
+    html.H4(children="My nice table"),
+    generate_table(DF_EN),
+])
+
+
+@APP.callback(
     [
-        html.Div(dcc.Input(id="input-box", type="text")),
-        html.Button("Submit", id="button"),
-        html.Div(
-            id="output-container-button", children="Enter a value and press submit"
-        ),
-        html.H4(children="US Agriculture Exports (2011)"),
-        generate_table(df),
-    ]
-)
-
-
-@app.callback(
-    dash.dependencies.Output("output-container-button", "children"),
-    [dash.dependencies.Input("button", "n_clicks")],
+        dash.dependencies.Output("table", "columns"),
+        dash.dependencies.Output("table", "data")
+    ],
+    [dash.dependencies.Input('button', 'n_clicks')],
     [dash.dependencies.State("input-box", "value")],
 )
 def update_output(n_clicks, value):
-    return 'The input value was "{}" and the button has been clicked {} times'.format(
-        value, n_clicks
-    )
+    """Update table"""
+    if value == "fr":
+        columns = [{"name": i, "id": i} for i in DF_FR.columns]
+        data = DF_FR.to_dict('records')
+        return columns, data
+    else:
+        columns = [{"name": i, "id": i} for i in DF_EN.columns]
+        data = DF_EN.to_dict('records')
+        return columns, data
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    APP.run_server(debug=True)
